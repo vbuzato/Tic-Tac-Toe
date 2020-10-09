b54845d782447deb910e64ae217d535c13d15314
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-10-09 16:23:40 -0300

    Updates

---
+++ precache-manifest.a43d1b5727bccbc779e19bde8f647006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213e8ea16fd9323b831499a83ca5bc13",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "1b73bb2f8d87c9c2a7f8",
+    "url": "/tic-tac-toe/static/css/main.aaa6eb1b.chunk.css"
+  },
+  {
+    "revision": "f40368803193043a8c8b",
+    "url": "/tic-tac-toe/static/js/2.e85b9851.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.e85b9851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b73bb2f8d87c9c2a7f8",
+    "url": "/tic-tac-toe/static/js/main.17d5a113.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
