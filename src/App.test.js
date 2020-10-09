4c1aed92406230903dd8e9b293450f1b568c61fd
Vinicius Buzato <viniciusbuzato@gmail.com>
2020-10-08 22:40:44 -0300

    Iniciando os testes

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Verifica se as funções do tabuleiro estão OK', () => {
+  test('Testa se todos o board inicia vazio', () => {
+    // const { getByText } = render(<App />);
+    // expect(getByText(/X/i)).toBeUndefined();
+    // expect(getByText(/O/i)).not.toBeInTheDocument();
+  });
+})
